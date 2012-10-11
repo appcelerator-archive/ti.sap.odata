647b57101e9da7ef339015d4627f362453fdfbeb
Evgenii <etcarev@appcelerator.com>
2012-10-11 12:16:39 -0700

    testing git commit

--- anvil/configSet/Resources/suites/sapodata.js
+++ anvil/configSet/Resources/suites/sapodata.js
@@ -9,6 +9,7 @@ module.exports = new function () {
     var finish;
     var valueOf;
     var SAPOData;
+    var testForGitPush;
     this.init = function (testUtils) {
         finish = testUtils.finish;
         valueOf = testUtils.valueOf;
